53569b9b987dddf140c61eb9f93c19d858687ada
Brayan1425 <123999442+Brayan1425@users.noreply.github.com>
2024-02-29 08:31:23 -0500

    areatriangulo

---
+++ areatriaungulo.py
@@ -0,0 +1,4 @@
+base = float(input("Ingrese la medida de la base"))
+altura = float(input("Ingrese la medida de la altura"))
+area_del_triangulo =  0.5 * (base) * (altura)
+print(area_del_triangulo)
